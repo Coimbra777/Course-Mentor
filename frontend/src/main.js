8d64988b8558f1f617fd5ba81d1ce3add1497ec5
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-02-22 14:04:25 -0300

    criação pagina de login e autenticação do usuário parte-1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./config/router";
 import store from "./config/store";
-import axios from "axios";
-
-axios.defaults.headers.common["Authorization"] =
-  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkdhYnJpZWwiLCJlbWFpbCI6ImdhYnJpZWxAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTcwODU2ODExMSwiZXhwIjoxNzA4ODI3MzExfQ.tDTPKZTI2afU7qcWKsy9T-kd2Wh4mruaEtRGQ_tPmR0";
 
 const app = createApp(App);
 
