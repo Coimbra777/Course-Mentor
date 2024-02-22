66e09b0fefc83a1ea521d72e7edc5c3dfff952b3
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-02-21 23:41:26 -0300

    criação StatiPage , area das estatísticas do projeto , falta criar a rota para consultar os dados vindo do mongoBD

---
+++ frontend/src/components/auth/Auth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="auth-content">
+    <div class="auth-modal"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthPage",
+};
+</script>
+
+<style>
+.auth-content {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.auth-modal {
+  width: 500px;
+  background-color: white;
+  padding: 50px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
--- frontend/src/components/home/HomePage.vue
+++ frontend/src/components/home/HomePage.vue
@@ -1,15 +1,52 @@
 <template>
   <div class="home">
-    <HomePage icon="fa fa-home" main="Dashboard" sub="Base de Conhecimento" />
+    <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de Conhecimento" />
+    <div class="stats">
+      <Stat
+        title="Categorias"
+        :value="stat.categories"
+        icon="fa fa-folder"
+        color="#d54d50"
+      />
+      <Stat
+        title="Artigos"
+        :value="stat.articles"
+        icon="fa fa-file"
+        color="#3bc480"
+      />
+      <Stat
+        title="Usuários"
+        :value="stat.users"
+        icon="fa fa-user"
+        color="#3282cd"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HomePage from "../home/HomePage.vue";
+import PageTitle from "../template/PageTitle.vue";
+import Stat from "./StatPage.vue";
+import axios from "axios";
+import { baseApiUrl } from "../../global";
 export default {
   name: "MainPage",
   components: {
-    HomePage,
+    PageTitle,
+    Stat,
+  },
+  data: function () {
+    return {
+      stat: {},
+    };
+  },
+  methods: {
+    getStats() {
+      axios.get(`${baseApiUrl}/stats`).then((res) => (this.stat = res.data));
+    },
+  },
+  mounted() {
+    this.getStats();
   },
 };
 </script>
