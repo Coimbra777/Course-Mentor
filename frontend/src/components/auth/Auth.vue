cfe0e07c7b281db291986d6c33cab6ea3697fe5b
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2024-02-26 19:23:18 -0300

    pagina de login e cadastro - logica e uso do vue-toastification para exibir as notificações das requisições

--- frontend/src/components/auth/Auth.vue
+++ frontend/src/components/auth/Auth.vue
@@ -2,68 +2,108 @@
   <div class="auth-content">
     <div class="auth-modal">
       <h2>{{ showSignup ? "Cadastro" : "Login" }}</h2>
-      <form @submit.prevent="submitForm">
-        <div v-if="showSignup" class="form-group">
-          <label for="username">Nome de usuário:</label>
-          <input type="text" id="username" v-model="user.username" required />
-        </div>
-        <div class="form-group">
-          <label for="email">E-mail:</label>
-          <input type="email" id="email" v-model="user.email" required />
-        </div>
-        <div class="form-group">
-          <label for="password">Senha:</label>
-          <input
-            type="password"
-            id="password"
-            v-model="user.password"
-            required
-          />
-        </div>
-        <div v-if="showSignup" class="form-group">
-          <label for="confirm-password">Confirmar Senha:</label>
-          <input
-            type="password"
-            id="confirm-password"
-            v-model="user.confirmPassword"
-            required
-          />
-        </div>
-        <button type="submit">{{ showSignup ? "Cadastrar" : "Entrar" }}</button>
-        <p>
-          {{ showSignup ? "Já possui uma conta?" : "Não possui uma conta?" }}
-          <a href="#" @click="toggleSignup">{{
-            showSignup ? "Entrar" : "Cadastrar"
-          }}</a>
-        </p>
-      </form>
+      <div v-if="showSignup" class="form-group">
+        <label for="username">Nome de usuário:</label>
+        <input type="text" id="username" v-model="user.name" />
+      </div>
+      <div class="form-group">
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" v-model="user.email" />
+      </div>
+      <div class="form-group">
+        <label for="password">Senha:</label>
+        <input type="password" id="password" v-model="user.password" />
+      </div>
+      <div v-if="showSignup" class="form-group">
+        <label for="confirm-password">Confirmar Senha:</label>
+        <input
+          type="password"
+          id="confirm-password"
+          v-model="user.confirmPassword"
+        />
+      </div>
+      <div>
+        <button v-if="showSignup" @click="signup">Registrar</button>
+        <button v-else @click="signin">Entrar</button>
+      </div>
+      <p>
+        {{ showSignup ? "Já possui uma conta?" : "Não possui uma conta?" }}
+        <a href="#" @click="toggleSignup">{{
+          showSignup ? "Entrar" : "Cadastrar"
+        }}</a>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-// import { baseApiUrl, userKey } from "../../global";
+import { baseApiUrl, userKey } from "../../global";
+import axios from "axios";
+import { useToast } from "vue-toastification";
+
 export default {
   name: "AuthPage",
-  data: function () {
+  data() {
     return {
       showSignup: false,
       user: {},
+      toast: null,
     };
   },
   methods: {
     toggleSignup() {
       this.showSignup = !this.showSignup;
     },
+    signin() {
+      axios
+        .post(`${baseApiUrl}/signin`, this.user)
+        .then((res) => {
+          this.$store.commit("setUser", res.data);
+          localStorage.setItem(userKey, JSON.stringify(res.data));
+          this.$router.push({ path: "/" });
+          this.showToast("Login bem-sucedido", "success");
+        })
+        .catch((error) => {
+          if (error.response.data) {
+            this.showToast(error.response.data, "error");
+            console.log(error.response.data);
+          } else {
+            this.showToast("Erro ao fazer login", "error");
+          }
+        });
+    },
+
+    signup() {
+      axios
+        .post(`${baseApiUrl}/signup`, this.user)
+        .then(() => {
+          this.showToast("Usuário cadastrado com sucesso", "success");
+          this.user = {};
+          this.showSignup = false;
+        })
+        .catch((error) => {
+          if (error.response && error.response.data) {
+            this.showToast(error.response.data, "error");
+            console.log(error);
+          } else {
+            this.showToast("Erro ao cadastrar o usuário", "error");
+          }
+        });
+    },
+
+    showToast(message, type) {
+      this.toast(message, { type: type });
+    },
     submitForm() {
       if (this.showSignup) {
-        // Lógica para cadastro
-        console.log("Cadastro:", this.user);
+        this.signup();
       } else {
-        // Lógica para login
-        console.log("Login:", this.user);
+        this.signin();
       }
     },
+  },
+  mounted() {
+    this.toast = useToast();
   },
 };
 </script>
