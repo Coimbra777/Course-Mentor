1426039182a5bf5e30b6568afe73ddfd0b828539
Coimbra777 <gabrielfelipe.coimbra@gmail.com>
2024-05-01 18:42:26 -0300

    fix: ajuste na migration da tabela users para inserir um usuario admin ao iniciar o projeto

--- backend/migrations/20240125122059_create_table_users.js
+++ backend/migrations/20240125122059_create_table_users.js
@@ -1,11 +1,22 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", (table) => {
-    table.increments("id").primary();
-    table.string("name").notNull();
-    table.string("email").notNull().unique();
-    table.string("password").notNull();
-    table.boolean("admin").notNull().defaultTo(false);
-  });
+  return knex.schema
+    .createTable("users", (table) => {
+      table.increments("id").primary();
+      table.string("name").notNull();
+      table.string("email").notNull().unique();
+      table.string("password").notNull();
+      table.boolean("admin").notNull().defaultTo(false);
+    })
+    .then(() => {
+      return knex("users").insert([
+        {
+          name: "admin",
+          email: "admin@admin.com.br",
+          password: "admin",
+          admin: true,
+        },
+      ]);
+    });
 };
 
 exports.down = function (knex) {
